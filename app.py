bc5026b276653ebf5abdd2400168b8d97b4c85c8
shubhamjangid1995 <141934224+shubhamjangid1995@users.noreply.github.com>
2023-08-16 19:09:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-
-
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-
-
 from sklearn.feature_extraction.text import CountVectorizer
-
 from sklearn.metrics.pairwise import cosine_similarity
-
 import requests
 
 
